c6dab3c3ae684337bf4d9bfee1655b6c612e0ce0
Aleph017 <turdiev1@gmail.com>
2025-04-20 17:12:06 +0300

    Update apiframe.py
    Some cursed refactoring done and colorless flag added.

--- apiframe.py
+++ apiframe.py
@@ -10,74 +10,104 @@ colors = {
     "clear" : "\x1b[0m"
 }
 
-yellow = "\x1b[0;34;33m"
-
 def getFactionColor(faction):
+    
     return colors.get(faction, colors["clear"])
 
-print("Fetching data...", end="\r")
+def NGEcheck(string):
+    return "Negative Time Error" if '-' in string else string
 
-request = requests.get(f"https://api.warframestat.us/pc/")
+def requestInfo():
+    request = requests.get("https://api.warframestat.us/pc/")
+    if request.status_code != 200:
+        print(f"Failed to get info from Warframestat API: {request.status_code}", 
+              file=sys.stderr)
+        sys.exit(1)
+    return request.json()
 
-if request.status_code != 200:
-    print(f"Failed getting info from Warframestat API: {request.status_code}", file=sys.stderr)
-    sys.exit(1)
-
-response = request.json()
-
-cycles = {
+def printCycles(response):
+    cycles = {
     "venusInfo" : {
         "info" : response["vallisCycle"],
         "name" : "Orb Vallis (Venus)"
-    },
+        },
     "cetusInfo" : {
         "info" : response["cetusCycle"],
         "name" : "Plains of Eidolon (Earth)"
-    },
+        },
     "deimosInfo" : {
         "info" : response["cambionCycle"],
         "name" : "Cambion Drift (Deimos)"
-    },
+        },
     "duviriInfo" : {
         "info" : response["duviriCycle"],
         "name" : "Duviri"
+        }
     }
-}
 
-baroInfo = response["voidTrader"]
-
-invasions = response["invasions"]
-
-for key, cycle in cycles.items():
-    if cycle['name'] != "Duviri":
-        eta = ""
-        if cycle['name'] != "Duviri" and '-' in cycle['info']['timeLeft']:
-            eta = "Negative Time Error"
+    for key, cycle in cycles.items():
+        if cycle['name'] != "Duviri":
+            eta = ""
+            if cycle['name'] != "Duviri" and '-' in cycle['info']['timeLeft']:
+                eta = "Negative Time Error"
+            else:
+                eta = cycle['info']['timeLeft']
+            print(f"{cycle['name']}: {cycle['info']['state'].title()}, {eta} left;") 
         else:
-            eta = cycle['info']['timeLeft']
-        print(f"{cycle['name']}: {cycle['info']['state'].title()}, {eta} left;") 
+            print(f"{cycle['name']}: {cycle['info']['state'].title()};")
+
+def printBaro(response):
+    baroInfo = response["voidTrader"]
+    if baroInfo["active"]:
+        print(f"\nVoid Trader: {baroInfo['location']}, {NGEcheck(baroInfo['endString'])} left;")
     else:
-        print(f"{cycle['name']}: {cycle['info']['state'].title()};")
-    
-if baroInfo["active"]:
-    print(f"\nVoid Trader: {baroInfo['location']}, {baroInfo['endString']} left;")
-else:
-    print(f"\nVoid Trader: will be active in {baroInfo['startString']} at {baroInfo['location']};")
+        print(f"\nVoid Trader: will be active in {NGEcheck(baroInfo['startString'])} at {baroInfo['location']};")
+    return None
 
-print("\nActive invasions:")
-
-for invasion in invasions:
+def printInvasion(invasion, isColorful):
     if not invasion['completed']:
         node = invasion['nodeKey']
         attacker = invasion['attacker']['factionKey']
         defender = invasion['defender']['factionKey']
-        eta = "Negative Time Error" if '-' in invasion['eta'] else invasion['eta']
+        attackReward = "" if invasion['vsInfestation'] else invasion['attacker']['reward']['asString']
         defenderReward = invasion['defender']['reward']['asString']
-        attackerColor = getFactionColor(attacker)
-        defenderColor = getFactionColor(defender)
-        
+        attackerColor = "" if not isColorful else getFactionColor(attacker)
+        defenderColor = "" if not isColorful else getFactionColor(defender)
+        yellow = "" if not isColorful else "\x1b[0;34;33m"
+        resetANSI = "" if not isColorful else "\x1b[0m"
+        eta = NGEcheck(invasion['eta'])
+
         if not invasion['vsInfestation']:
-            attackReward = invasion['attacker']['reward']['asString']
-            print(f"\t{node}:\n\t\t{attackerColor}{attacker}\x1b[0m:\n\t\t- Reward: {yellow}{attackReward}\x1b[0m,\n\t\t{defenderColor}{defender}\x1b[0m:\n\t\t- Reward: {yellow}{defenderReward}\x1b[0m,\n\t\t{eta} left;\n")
+            print(f"\t{node}:\n\t\t{attackerColor}{attacker}{resetANSI}:\n\t\t- Reward: {yellow}{attackReward}{resetANSI},\n\t\t{defenderColor}{defender}{resetANSI}:\n\t\t- Reward: {yellow}{defenderReward}{resetANSI},\n\t\t{eta} left;\n")
         else:
-            print(f"\t{node}:\n\t\t{attackerColor}{attacker}\x1b[0m,\n\t\t{defenderColor}{defender}\x1b[0m:\n\t\t- Reward: {yellow}{defenderReward}\x1b[0m,\n\t\t{eta} left;\n")
+            print(f"\t{node}:\n\t\t{attackerColor}{attacker}{resetANSI},\n\t\t{defenderColor}{defender}{resetANSI}:\n\t\t- Reward: {yellow}{defenderReward}{resetANSI},\n\t\t{eta} left;\n")
+
+
+
+def printInvasions(response, isColorful):
+    invasions = response["invasions"]
+    print("\nActive invasions:")
+    for invasion in invasions:
+        printInvasion(invasion, isColorful)
+    
+def printHelp():
+    print("usage: apiframe [-c | --colorless] [-h | --help]\n\n-c | --colorless: disables colors in output\n-h | --help: prints this message\n")
+    sys.exit(0)
+
+def main(args):
+    isColorful = True
+
+    for arg in args:
+        if arg == "-c" or arg == "--colorless":
+            isColorful = False
+        if arg == "-h" or "--help":
+            printHelp()
+
+    response = requestInfo()
+    printCycles(response)
+    printBaro(response)
+    printInvasions(response, isColorful)
+    return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
